8a6f3b0085693f64286c53d68462e779a57cfa2c
dinarwanjiru <dinarwanjiru77@gmail.com>
2023-07-24 16:12:30 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -4,8 +4,8 @@ void print_buffer(char buffer[], int *buff_ind);
 
 /**
  * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * @format: this is the format.
+ * Return:The Printed chars.
  */
 int _printf(const char *format, ...)
 {
